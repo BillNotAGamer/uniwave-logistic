8c3f15493ca37a2120e73d605499d4dcea5f0425
BillNotAGamer <cuthep64416@gmail.com>
2025-12-19 03:25:20 +0700

    update

--- js/api.js
+++ js/api.js
@@ -121,7 +121,7 @@ function setupProfileMenu() {
   const bodyEl = document.body;
 
   if (hasToken) {
-    profileLink.setAttribute("href", "/user-dashboard");
+    profileLink.setAttribute("href", "/vi/user-dashboard");
     if (bodyEl) bodyEl.classList.add("logged-in");
   } else {
     profileLink.setAttribute("href", "/vi/authentication");
