811d82340d95ae540f96c4c4e30c48db07d6a36c
BillNotAGamer <cuthep64416@gmail.com>
2026-02-04 15:48:09 +0700

    update

--- js/api.js
+++ js/api.js
@@ -5,7 +5,7 @@
  *   <script>window.API_BASE_URL = "https://api.example.com";</script>
  *   <script src="js/api.js"></script>
  */
-const BASE_API_URL = window.API_BASE_URL || "http://billnotagamer-001-site1.stempurl.com/ ";
+const BASE_API_URL = window.API_BASE_URL || "http://localhost:7258/";
 
 const ACCESS_TOKEN_KEY = "uniwave_access_token";
 const REFRESH_TOKEN_KEY = "uniwave_refresh_token";
